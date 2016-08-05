4a4c0f98eed9b92d054d08baf691a880e73c976e
Sofia <emmapohjalainen@gmail.com>
2016-08-05 14:39:01 +0100

    feat(react-component): ItemTitle component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-import ItemTitle from './ItemTitle.jsx';
-
 class App extends React.Component {
   render() {
     return (
-      <ItemTitle title="Hey!" />
+      <h1>Hello World!!!</h1>
     );
   }
 }
