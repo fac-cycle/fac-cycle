809185bcd2171c70a9705a70b4f9100509ca1d6f
Shadows666 <lifeloveregretsxe@gmail.com>
2016-08-05 13:13:01 +0100

    test(database): creates first user helper test

--- server/lib/add_user_database.js
+++ server/lib/add_user_database.js
@@ -1,44 +1,44 @@
-// const request = require('request');
-// const pgClient = require('./pg_client');
-//
-// const getGeoLocation = (postcode, callback) => {
-//   const base = 'https://api.postcodes.io/postcodes/';
-//   request(base + postcode, (err, response, body) => {
-//     if (err) callback(err);
-//     if (!err && response.statusCode === 200) {
-//       callback(err, body.result);
-//     }
-//   });
-// };
-//
-// const addUserDatabase = (connection, user, callback) => {
-//   const name = user.name;
-//   const email = user.email;
-//   const facebookId = user.facebookId;
-//   const profileImgUrl = user.profileImgUrl;
-//   const postcode = user.postCode;
-//   const client = pgClient(connection);
-//   getGeoLocation(postcode, (err, reply) => {
-//     const lat = reply.latitude;
-//     const lng = reply.longitude;
-//     client.query(
-//       `INSERT INTO users (name, email, facebook_id, profile_img_url, postcode, lat, lng)
-//       VALUES ($1, $2, $3, $4, $5, $6, $7)
-//       RETURNING id`,
-//       [name, email, facebookId, profileImgUrl, postcode, lat, lng],
-//     (postgresErr, result) => {
-//       if (postgresErr) {
-//         callback(postgresErr);
-//       } else {
-//         callback(postgresErr, result);
-//       }
-//       client.end();
-//     }
-//     );
-//   });
-// };
-//
-// module.exports = {
-//   addUserDatabase,
-//   getGeoLocation,
-// };
+const request = require('request');
+const pgClient = require('./pg_client');
+
+const getGeoLocation = (postcode, callback) => {
+  const base = 'https://api.postcodes.io/postcodes/';
+  request(base + postcode, (err, response, body) => {
+    if (err) callback(err);
+    if (!err && response.statusCode === 200) {
+      callback(err, body.result);
+    }
+  });
+};
+
+const addUserDatabase = (connection, user, callback) => {
+  const name = user.name;
+  const email = user.email;
+  const facebookId = user.facebookId;
+  const profileImgUrl = user.profileImgUrl;
+  const postcode = user.postCode;
+  const client = pgClient(connection);
+  getGeoLocation(postcode, (err, reply) => {
+    const lat = reply.latitude;
+    const lng = reply.longitude;
+    client.query(
+      `INSERT INTO users (name, email, facebook_id, profile_img_url, postcode, lat, lng)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id`,
+      [name, email, facebookId, profileImgUrl, postcode, lat, lng],
+    (postgresErr, result) => {
+      if (postgresErr) {
+        callback(postgresErr);
+      } else {
+        callback(postgresErr, result);
+      }
+      client.end();
+    }
+    );
+  });
+};
+
+module.exports = {
+  addUserDatabase,
+  getGeoLocation,
+};
