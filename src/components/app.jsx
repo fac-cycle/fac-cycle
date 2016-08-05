d301114000001b55191bdbcccd8644e0c3ec41b4
Nogaenbar <noga.inbar@gmail.com>
2016-08-05 15:18:50 +0100

    chore(imageComponent): changed name of image component file and changed app.jsx to default

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-
-import Image from './ItemImage.jsx';
 
 class App extends React.Component {
   render() {
     return (
-    <Image imgClass="imgClass" imgUrl="http://www.stokowski.org/images/Nikisch_BSO_1891_NH.jpg" />
-  ); }
+      <h1>Hello world</h1>
+    );
+  }
 }
 
 export default App;
