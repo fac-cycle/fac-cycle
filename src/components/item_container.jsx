931c9d3030c44c124e8e589ed95e1e7b1027f3b3
Sofia <emmapohjalainen@gmail.com>
2016-08-09 12:59:38 +0100

    chore(react-component): add homepage component and add ...this.props to all the relevant components

--- src/components/item_container.jsx
+++ src/components/item_container.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import Image from './item_image.jsx';
+import { Link } from 'react-router';
+import ItemImage from './item_image.jsx';
 import ItemTitle from './item_title.jsx';
+import ItemDescription from './item_description.jsx';
 
 class ItemContainer extends React.Component {
+
   render() {
     return (
-      <div className="itemContainer">
-        <Image imgClass={this.props.imgClass} imgUrl={this.props.imgUrl} />
-        <ItemTitle title={this.props.title} />
+      <div>
+        <Link to="/description">
+          <div className="itemContainer">
+            <ItemImage {...this.props} imgClass={this.props.imgClass} imgUrl={this.props.imgUrl} />
+            <ItemTitle {...this.props} title={this.props.title} />
+            <ItemDescription {...this.props} description={this.props.description} />
+          </div>
+        </Link>
       </div>
     );
   }
@@ -18,6 +26,7 @@ ItemContainer.propTypes = {
   imgClass: React.PropTypes.string,
   imgUrl: React.PropTypes.string,
   title: React.PropTypes.string,
+  description: React.PropTypes.string,
 };
 
 export default ItemContainer;
---
+++ src/components/item_description.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class ItemDescription extends React.Component {
+  render() {
+    return (
+      <span className="itemDescription">{this.props.description}</span>
+    );
+  }
+}
+
+ItemDescription.propTypes = {
+  description: React.PropTypes.string,
+};
+
+export default ItemDescription;
