0c0b209819b4bbb201dadf872cd807e7d31bac32
Nogaenbar <noga.inbar@gmail.com>
2016-08-09 17:05:22 +0100

    chore(postAdComponent): Add the postAd component

---
+++ src/components/dropbox.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+class Dropbox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      files: [],
+    };
+  }
+
+  onDrop(file) {
+    console.log('Recieved files: ', file);
+    this.setState({
+      files: this.state.files.concat(file),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Dropzone onDrop={() => { this.onDrop(); }}>
+          <div>Click here to access your Finder</div>
+        </Dropzone>
+        <p>These are the image previews</p>
+        <div>
+          {this.state.files.map((file, i) => <img
+            alt="bla"
+            style={{
+              height: '100px',
+              width: '100px',
+              display: 'inline-block',
+              margin: '5px',
+            }}
+            src={file.preview} key={i}
+          />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dropbox;
---
+++ src/components/post_ad_component.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import Dropbox from './dropbox.jsx';
+
+class PostAd extends React.Component {
+
+  addItem() {
+    const postcode = document.getElementById('postcode').value;
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const imageUrls = ['http://tinyurl.com/jdxmmrc', 'http://tinyurl.com/jag4bnx'];
+
+
+    this.props.store.dispatch({
+      type: 'ADD_ITEM',
+      async: false,
+      postcode,
+      title,
+      description,
+      imageUrls,
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navigator />
+        <div>
+          <input
+            {...this.props}
+            id="title"
+            className="titleInput"
+            type="text"
+            value={this.props.store.state.title}
+          />
+          <Dropbox />
+          <textarea
+            {...this.props}
+            id="description"
+            className="titleInput"
+            type="text"
+            value={this.props.store.state.description}
+          />
+          <input
+            {...this.props}
+            id="postcode"
+            className="titleInput"
+            type="text"
+            value={this.props.store.state.postcode}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+PostAd.propTypes = {
+  store: {
+    dispatch: React.PropTypes.dispatch,
+  },
+};
+
+export default PostAd;
